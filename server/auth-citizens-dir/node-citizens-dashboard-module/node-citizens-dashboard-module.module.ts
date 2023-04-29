aea58b3074f2065533e4724d256f1854f2492c90
WrappedUsername <wrappedusername@gmail.com>
2023-04-28 20:40:51 -0600

    Configuring files - Keefe

--- server/auth-citizens-dir/node-citizens-dashboard-module/node-citizens-dashboard-module.module.ts
+++ server/auth-citizens-dir/node-citizens-dashboard-module/node-citizens-dashboard-module.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NodeCitizensDashboardComponent } from './node-citizens-dashboard/node-citizens-dashboard.component';
 
-
-
 @NgModule({
   declarations: [
     NodeCitizensDashboardComponent
